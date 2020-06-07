3dff1a5e2865e65ef7dd2465e87b702b3f549ec0
MonsieurWave <tensu.wave@gmail.com>
2020-06-07 15:54:29 +0200

    Cleaned up implementation of torchio transformations

--- dataio/transformation/transforms.py
+++ dataio/transformation/transforms.py
@@ -12,28 +12,32 @@ class Transformations:
         self.name = name
 
         # Input patch and scale size
-        self.scale_size = (192, 192, 1)
-        self.patch_size = (128, 128, 1)
+        self.scale_size = (192, 192, 1)  # pad up to this shape
+        # Todo implement random crop augmentation
+        # self.patch_size = (128, 128, 1)
+
+        # Affine, Elastic, Flip and Noise transformations are forked from torchio
+        # Further documentation for all arguments can thus be found here:
+        # https://torchio.readthedocs.io/transforms/augmentation.html#augmentation
+        # Most values can either be given along every dimension (vx, vy, vz);
+        # or as a single value v if the same value should be applied along each dimension
+
 
         # Affine and Intensity Transformations
-        self.shift_val = (0.1, 0.1)
+        # self.shift_val = (0.1, 0.1) todo - evaluation relevance of shift augmentation
         self.rotate_val = 15.0
         self.scale_val = (0.7, 1.3)
-        self.inten_val = (1.0, 1.0)
+        self.flip_axis = (0)  # axes - Axis or tuple of axes along which the image will be flipped.
+        self.noise_std = (0, 0.25)  # range of noise std
+        # self.inten_val = (1.0, 1.0) - Todo implement intensitiy augmentation
+
+        self.max_deform = (7.5, 7.5, 7.5)  # Maximum deformation allowed for elastic transform
+        self.elastic_control_points = (7, 7, 7)  # control points along each dimension
+
         self.random_flip_prob = 0.0
         self.random_affine_prob = 0.0
         self.random_elastic_prob = 0.0
-
-        # Divisibility factor for testing
-        self.division_factor = (16, 16, 1)
-
-        # Maximum allowed for elastic transform
-        self.max_deform = (7.5, 7.5, 7.5)
-
-    def get_transformation(self):
-        return {
-            'gsd_pCT': self.get_gsd_pCT_transformer()
-        }[self.name]
+        self.random_noise_prob = 0.0
 
     def print(self):
         print('\n\n############# Augmentation Parameters #############')
@@ -46,58 +50,52 @@ class Transformations:
 
         # Affine and Intensity Transformations
         if hasattr(t_opts, 'scale_size'):       self.scale_size = t_opts.scale_size
-        if hasattr(t_opts, 'patch_size'):       self.patch_size = t_opts.patch_size
-        if hasattr(t_opts, 'shift_val'):        self.shift_val = t_opts.shift
+        # if hasattr(t_opts, 'patch_size'):       self.patch_size = t_opts.patch_size
+        # if hasattr(t_opts, 'shift_val'):        self.shift_val = t_opts.shift
         if hasattr(t_opts, 'rotate'):           self.rotate_val = t_opts.rotate
         if hasattr(t_opts, 'scale_val'):        self.scale_val = t_opts.scale_val
         if hasattr(t_opts, 'max_deform'):       self.max_deform = t_opts.max_deform
-        if hasattr(t_opts, 'inten_val'):        self.inten_val = t_opts.intensity
+        if hasattr(t_opts, 'elastic_control_points'):       self.elastic_control_points = t_opts.elastic_control_points
+        if hasattr(t_opts, 'flip_axis'):       self.flip_axis = t_opts.flip_axis
+        if hasattr(t_opts, 'noise_std'):       self.noise_std = t_opts.noise_std
+        # if hasattr(t_opts, 'inten_val'):        self.inten_val = t_opts.intensity
         if hasattr(t_opts, 'random_flip_prob'): self.random_flip_prob = t_opts.random_flip_prob
         if hasattr(t_opts, 'random_affine_prob'): self.random_affine_prob = t_opts.random_affine_prob
         if hasattr(t_opts, 'random_elastic_prob'): self.random_elastic_prob = t_opts.random_elastic_prob
-        if hasattr(t_opts, 'division_factor'):  self.division_factor = t_opts.division_factor
+        if hasattr(t_opts, 'random_noise_prob'): self.random_noise_prob = t_opts.random_noise_prob
 
-    def get_gsd_pCT_transformer(self):
-        return {'train': self.gsd_pCT_train_transform, 'valid': self.gsd_pCT_valid_transform}
+    def get_transformation(self):
+        '''
+        Get transformations for this dataset
+        :return:
+        '''
+        return {
+            'gsd_pCT': {'train': self.gsd_pCT_train_transform, 'valid': self.gsd_pCT_valid_transform}
+        }[self.name]
 
     def gsd_pCT_train_transform(self, seed=None):
         if seed is None:
-            # seed must be an integer for torch
-            seed = np.random.randint(0, 9999)
+            seed = np.random.randint(0, 9999)  # seed must be an integer for torch
 
         train_transform = ts.Compose([
             ts.ToTensor(),
             ts.Pad(size=self.scale_size),
             ts.TypeCast(['float', 'float']),
-            # ts.RandomFlip(h=True, v=True, p=self.random_flip_prob),
-            # RandomElasticTransform(seed=seed, max_output_channels=self.max_output_channels),
-            # RandomFlipTransform(axes=(0), p=self.random_flip_prob, seed=seed, max_output_channels=self.max_output_channels),
-            # RandomElasticTransform(seed=seed, p=1, image_interpolation=Interpolation.BSPLINE, max_displacement=self.max_deform,
-            #                        max_output_channels=self.max_output_channels),
+            RandomFlipTransform(axes=self.flip_axis, p=self.random_flip_prob, seed=seed, max_output_channels=self.max_output_channels),
+            RandomElasticTransform(seed=seed, p=self.random_elastic_prob, image_interpolation=Interpolation.BSPLINE, max_displacement=self.max_deform,
+                                   num_control_points=self.elastic_control_points,  max_output_channels=self.max_output_channels),
             RandomAffineTransform(scales = self.scale_val, degrees = (self.rotate_val), isotropic = True, default_pad_value = 0,
                         image_interpolation = Interpolation.BSPLINE, seed=seed, p=self.random_affine_prob, max_output_channels=self.max_output_channels),
-            # RandomNoiseTransform(p=0.5, seed=seed, max_output_channels=self.max_output_channels),
-            # Todo Random Affine doesn't support channels --> try newer version of torchsample or torchvision
-            # ts.RandomAffine(rotation_range=self.rotate_val, translation_range=self.shift_val,
-            #                 zoom_range=self.scale_val, interp=('bilinear', 'nearest')),
+            RandomNoiseTransform(p=self.random_noise_prob, std=self.noise_std, seed=seed, max_output_channels=self.max_output_channels),
             ts.ChannelsFirst(),
-            #ts.NormalizeMedicPercentile(norm_flag=(True, False)),
+            # ts.NormalizeMedicPercentile(norm_flag=(True, False)),
             # Todo apply channel wise normalisation
             ts.NormalizeMedic(norm_flag=(True, False)),
-            # Todo fork torchsample and fix the Random Crop bug
+            # Todo eventually add random crop augmentation (fork torchsample and fix the Random Crop bug)
             # ts.ChannelsLast(), # seems to be needed for crop
             # ts.RandomCrop(size=self.patch_size),
             ts.TypeCast(['float', 'long'])
         ])
-
-        # train_transform = tv.Compose([
-        #     tv.Lambda(lambda a: torch.from_numpy(a)),
-        #     ts.Pad(size=self.scale_size),
-        #     # tv.Lambda(lambda a: tv.Pad(a, self.get_padding(a))),
-        #     # it.PadToScale(scale_size=self.scale_size),
-        #     tv.Lambda(lambda a: a.permute(3, 0, 1, 2)),
-        #     tv.Lambda(lambda a: a.float()),
-        # ])
 
         return train_transform
 
@@ -114,51 +112,4 @@ class Transformations:
             ts.TypeCast(['float', 'long'])
         ])
 
-        # valid_transform = tv.Compose([
-        #     tv.Lambda(lambda a: torch.from_numpy(a)),
-        #     ts.Pad(size=self.scale_size),
-        #     # tv.Lambda(lambda a: tv.Pad(a, self.get_padding(a))),
-        #     # it.PadToScale(scale_size=self.scale_size),
-        #     tv.Lambda(lambda a: a.permute(3, 0, 1, 2)),
-        #     tv.Lambda(lambda a: a.float()),
-        #
-        # ])
-
         return valid_transform
-
-    # def gsd_pCT_transform(self, seed=None):
-    #     '''
-    #     Data augmentation transformations for the Geneva Stroke dataset (pCT maps)
-    #     :return:
-    #     '''
-    #     if seed is None:
-    #         seed = np.random.rand()
-    #     print('yoooooooo', seed)
-    #
-    #     train_transform = ts.Compose([
-    #         ts.ToTensor(),
-    #         ts.Pad(size=self.scale_size),
-    #         ts.TypeCast(['float', 'float']),
-    #         # ts.RandomFlip(h=True, v=True, p=self.random_flip_prob),
-    #         RandomFlipTransform(axes=(0), p=self.random_flip_prob, seed=seed, max_output_channels=self.max_output_channels),
-    #         RandomElasticTransform(seed=seed, p=0.5, image_interpolation=Interpolation.BSPLINE, max_displacement=self.max_deform,
-    #                                max_output_channels=self.max_output_channels),
-    #         RandomAffineTransform(scales = self.scale_val, degrees = (self.rotate_val), isotropic = False, default_pad_value = 0,
-    #                     image_interpolation = Interpolation.BSPLINE, seed=seed, p=0.5, max_output_channels=self.max_output_channels),
-    #         RandomNoiseTransform(p=0.5, seed=seed, max_output_channels=self.max_output_channels),
-    #         # Todo Random Affine doesn't support channels --> try newer version of torchsample or torchvision
-    #         # ts.RandomAffine(rotation_range=self.rotate_val, translation_range=self.shift_val,
-    #         #                 zoom_range=self.scale_val, interp=('bilinear', 'nearest')),
-    #         ts.ChannelsFirst(),
-    #         #ts.NormalizeMedicPercentile(norm_flag=(True, False)),
-    #         # Todo apply channel wise normalisation
-    #         ts.NormalizeMedic(norm_flag=(True, False)),
-    #         # Todo fork torchsample and fix the Random Crop bug
-    #         # ts.ChannelsLast(), # seems to be needed for crop
-    #         # ts.RandomCrop(size=self.patch_size),
-    #         ts.TypeCast(['float', 'long'])
-    #     ])
-    #
-    #
-    #
-    #     return {'train': train_transform, 'valid': valid_transform}
